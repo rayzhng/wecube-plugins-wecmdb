8643b906f401bdcf7773d827487f5229ae908047
Jordan Zhang <jordanzhang@webank.com>
2019-11-05 18:18:42 +0800

    #14 Pass on role name from the wecube to wecmdb

---
+++ src/main/java/com/webank/plugins/wecmdb/interceptor/RestTemplateInterceptor.java
@@ -0,0 +1,22 @@
+package com.webank.plugins.wecmdb.interceptor;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestTemplateInterceptor implements ClientHttpRequestInterceptor {
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+            throws IOException {
+        HttpHeaders headers = request.getHeaders();
+        String roleName= RoleStorage.getIntance().get();
+        headers.add("roleName", roleName);
+        return execution.execute(request, body);
+    }
+}
