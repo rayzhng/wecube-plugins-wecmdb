c593b9c3beaed5529688ea6e95bf66c227852527
Jordan Zhang <jordanzhang@webank.com>
2019-11-28 19:21:29 +0800

    #37 Extract the ciTypeId from guid

--- src/main/java/com/webank/plugins/wecmdb/dto/OperateCiDto.java
+++ src/main/java/com/webank/plugins/wecmdb/dto/OperateCiDto.java
@@ -2,15 +2,6 @@ package com.webank.plugins.wecmdb.dto;
 
 public class OperateCiDto {
     private String guid;
-    private String entityName;
-
-    public String getEntityName() {
-        return entityName;
-    }
-
-    public void setEntityName(String entityName) {
-        this.entityName = entityName;
-    }
 
     public String getGuid() {
         return guid;
--- src/main/java/com/webank/plugins/wecmdb/support/cmdb/dto/CmdbOperateCiDto.java
+++ src/main/java/com/webank/plugins/wecmdb/support/cmdb/dto/CmdbOperateCiDto.java
@@ -29,4 +29,8 @@ public class CmdbOperateCiDto {
         this.ciTypeId = ciTypeId;
     }
 
+    @Override
+    public String toString() {
+        return "CmdbOperateCiDto [guid=" + guid + ", ciTypeId=" + ciTypeId + "]";
+    }
 }
